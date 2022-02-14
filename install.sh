de6f21fe44296e034a26ac2999656742bf77ae42
Dominik Hasselkuss <dh@we-push-it.com>
2022-02-14 21:35:02 +0100

    added git clone

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ echo "start install."
 
 apt-get update
 apt-get upgrade -y
-apt-get install vim gnupg2 curl wget python3 python3-pip -y
+apt-get install vim gnupg2 curl wget python3 python3-pip git -y
 pip3 install ansible
 
 ansible --version
 
-
+git clone git@github.com:we-push-it/workstation.git
