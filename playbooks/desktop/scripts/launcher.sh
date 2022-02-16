5d103889d62278250dea93d5802bdbf61f92efbe
Dominik Haßelkuss <Domi-cc@users.noreply.github.com>
2022-02-16 14:43:39 +0100

    initial setup

--- playbooks/desktop/scripts/launcher.sh
+++ playbooks/desktop/scripts/launcher.sh
@@ -1,1 +1,11 @@
-.
+#!/bin/bash
+
+# kill old instances of polybar
+
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+
+# run one or more instances. you can run more bars for every display here
+
+polybar bar1 2>&1 | tee -a /tmp/polybar-bar1.log & disown
